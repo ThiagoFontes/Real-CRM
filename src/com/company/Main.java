21ed4cd1c35c80fa82af467718039cbde0230ad8
Thiago dos Santos Fontes <thiago93br@gmail.com>
2018-08-04 22:58:09 -0300

    Menu remade with listeners added, new contatos window

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,13 @@
 package com.company;
-import com.company.inc.*;
-import com.company.inc.Menu;
+import com.company.inc.ComponentMover;
+import com.company.inc.customBtn;
+import com.company.telas.TelaContatos;
 import com.company.telas.TelaInicial;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.EventListener;
 
 /**
  * Defines the main frame that will be suporting each window content,
@@ -12,6 +15,10 @@ import java.awt.*;
  */
 public class Main extends JFrame {
     private String menuStrings[] = {"Início", "Contatos", "Negociações", "Relacionamento"};
+    private JPanel content = new JPanel(new BorderLayout());
+    private JLabel[] label = new JLabel[menuStrings.length];
+    private JPanel menu = new JPanel();
+    private int current = 0;
 
     public Main () {
         super.setLayout(new BorderLayout());
@@ -41,23 +48,97 @@ public class Main extends JFrame {
         top.add(m);
         top.add(closeBtn);
 
+        //criando menu
+        menu.setLayout( new FlowLayout(FlowLayout.RIGHT));
+        for (int i = 0; i < menuStrings.length; i++) {
+            label[i] = new JLabel();
+            label[i].setText(menuStrings[i]);
+            label[i].setForeground(Color.BLACK);
+            label[i].setHorizontalAlignment(JLabel.CENTER);
+            //Começa na página inicial
+            if (i == current) {
+                label[i].setForeground(Color.GRAY);
+            }
+
+            label[i].setPreferredSize(new Dimension(110, 30));
+            //label[i].setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+            label[i].setCursor(new Cursor(Cursor.HAND_CURSOR));
+            label[i].addMouseListener(ml);
+            menu.add(label[i]);
+        }
+        content.add(menu, BorderLayout.NORTH);
+
+
         // Adicionando componentes a janela principal
+
         super.add("North", top);
         super.setUndecorated(true);
         super.getRootPane().setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, Color.BLACK));
         super.setSize(800, 500);
+        super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         super.setLocationRelativeTo(null);
         super.setVisible(true);
 
     }
 
     private  void setTelaInicial () {
-        JPanel content = new JPanel(new BorderLayout());
-        content.add("North", new Menu(menuStrings, 0));
         content.add("Center", new TelaInicial());
         this.add("Center", content);
         super.setVisible(true);
     }
+
+    private  void setTelaContatos () {
+        content.add("Center", new TelaContatos());
+        this.add("Center", content);
+        super.setVisible(true);
+    }
+    MouseListener ml = new MouseListener() {
+
+        private void refreshColors(MouseEvent e){
+            for (int i = 0; i < menuStrings.length; i++) {
+                if((e.getSource() == label[i]) && (i != current)) {
+                    label[i].setForeground(Color.black);
+                } else if (i == current) {
+                    label[i].setForeground(Color.GRAY);
+                }
+            }
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            for (int k = 0; k < menuStrings.length; k++) {
+                if(e.getSource() == label[k]) {
+                    current = k;
+                    refreshColors(e);
+                    setTelaContatos();
+                }
+            }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            refreshColors(e);
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            refreshColors(e);
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            for (int i = 0; i < menuStrings.length; i++) {
+                if(e.getSource() == label[i]) {
+                    label[i].setForeground(new Color(0x424345));
+                }
+            }
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            refreshColors(e);
+        }
+    };
 
     public static void main(String[] args) {
         Main tela = new Main();
---
+++ src/com/company/telas/TelaContatos.java
@@ -0,0 +1,24 @@
+package com.company.telas;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Thiago Fontes on 04/08/2018.
+ */
+public class TelaContatos extends JPanel {
+    String[] contatos = { "Bird", "Cat", "Dog", "Rabbit", "Pig", "Cat", "Dog", "Rabbit", "Pig", "Cat", "Dog", "Rabbit", "Pig", "Cat", "Dog", "Rabbit", "Pig", "Cat", "Dog", "Rabbit", "Pig", "Cat", "Dog", "Rabbit", "Pig", "Cat", "Dog", "Rabbit", "Pig" };
+    JScrollPane scrollPane;
+    public TelaContatos ()  {
+        super.setLayout(new BorderLayout());
+
+        JList contatoList = new JList(contatos);
+
+        scrollPane = new JScrollPane(contatoList);
+        scrollPane.setPreferredSize(new Dimension(200, 300));
+
+        super.add(scrollPane, BorderLayout.WEST);
+        super.setBorder(BorderFactory.createMatteBorder(30, 30, 30, 30, UIManager.getColor ( "Panel.background" )));
+        //super.add(sub, BorderLayout.PAGE_END);
+    }
+}
