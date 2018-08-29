edbb353925690a9915d9d41509f30ddf4efea0f3
khypra <khypra007@gmail.com>
2018-08-28 23:34:13 -0300

    Classe controle Relacionamento criada Classe Controle Negociacao criada
    falta implementar os metodos
    
    Signed-off-by: khypra <khypra007@gmail.com>

---
+++ src/Classes/ControleNegociacao.java
@@ -0,0 +1,23 @@
+package Classes;
+
+public class ControleNegociacao {
+    InteractDAO interact = new InteractDAO();
+
+    //metodods de Controle
+
+    public boolean cadastrarNegociacao(){
+        return false;
+    }
+
+    public boolean editarNegociacao(){
+        return false;
+    }
+
+    public boolean removerNegociacao(){
+        return false;
+    }
+
+    public void listarNegociacoes(){
+
+    }
+}
---
+++ src/Classes/ControleRelacionamento.java
@@ -0,0 +1,24 @@
+package Classes;
+
+public class ControleRelacionamento {
+    InteractDAO interact = new InteractDAO();
+
+    //metodods de Controle
+
+    public boolean cadastrarRelacionamento(){
+        return false;
+    }
+
+    public boolean editarRelacionamento(){
+        return false;
+    }
+
+    public boolean removerRelacionamento(){
+        return false;
+    }
+
+    public void listarRelacionamentos(){
+
+    }
+
+}
